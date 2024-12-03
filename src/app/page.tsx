5c462c9c6b4d0e753e536e8c3e9b8ac820c541d1
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-12-03 18:01:28 -0300

    Estilizando os produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
 import { fetchProducts } from "@/lib/api";
+import { ShoppingCart } from "lucide-react";
 
 export default async function Home() {
   const products = await fetchProducts()
@@ -12,9 +14,16 @@ export default async function Home() {
           {products.map((product) => (
             <li key={product.id} className="flex flex-col items-center gap-3 border my-10 p-2 rounded-lg">
               <h2 className="text-xl line-clamp-1">{product.title}</h2>
-              <img className="size-40 border p-2" src={product.image} alt={product.title} />
+              <div className="w-full max-w-[200px] rounded-lg">
+                <img src={product.image} alt={product.title} />
+              </div>
               <p className="line-clamp-3 text-slate-500">{product.description}</p>
-              <p className="font-semibold">$ {product.price}</p>
+              <div className="flex items-center justify-around w-full">
+                <p className="font-semibold">$ {product.price}</p>
+                <Button>
+                  <ShoppingCart />
+                </Button>
+              </div>
             </li>
           ))}
         </ul>
