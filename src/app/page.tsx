9a8b34311c969e49b0b4cf6269cbb85856e67a09
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-12-05 13:45:25 -0300

    Estilização da imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { fetchProducts } from "@/lib/api";
-import { ShoppingCart } from "lucide-react";
+import { Plus, ShoppingCart } from "lucide-react";
 
 export default async function Home() {
   const products = await fetchProducts()
@@ -9,20 +9,40 @@ export default async function Home() {
   return (
     <div>
       <Header />
-      <main>
+      <main className="mx-4">
         <ul>
           {products.map((product) => (
-            <li key={product.id} className="flex flex-col items-center gap-3 border my-10 p-2 rounded-lg">
+            <li key={product.id} className="flex flex-col items-center justify-center gap-4 border my-10 p-2 rounded-lg shadow-md size-96">
               <h2 className="text-xl line-clamp-1">{product.title}</h2>
-              <div className="w-full max-w-[200px] rounded-lg">
-                <img src={product.image} alt={product.title} />
+              <div>
+                <img className="w-full h-40" src={product.image} alt={product.title} />
               </div>
-              <p className="line-clamp-3 text-slate-500">{product.description}</p>
+              <p className="line-clamp-2 text-slate-500 indent-8 px-2">{product.description}</p>
               <div className="flex items-center justify-around w-full">
-                <p className="font-semibold">$ {product.price}</p>
-                <Button>
-                  <ShoppingCart />
-                </Button>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className="bg-slate-200 p-3 rounded-full">
+                      <Plus />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Mais informações</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+
+                <p className="font-semibold bg-slate-300 p-3 rounded-full text-slate-800 text-xl">$ {product.price}</p>
+
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className="bg-slate-200 p-3 rounded-full">
+                      <ShoppingCart />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Adicionar ao carrinho</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+
               </div>
             </li>
           ))}
