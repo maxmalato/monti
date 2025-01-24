d0c6fc10328bb127d1f9d635b732093ef8fabc7e
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-01-24 10:33:16 -0300

    API finalizada.

---
+++ app/services/api.ts
@@ -0,0 +1,24 @@
+const BASE_URL = "https://dummyjson.com"
+
+// API para buscar todos os produtos
+export const fetchProducts = async () => {
+    const response = await fetch(`${BASE_URL}/products`)
+
+    if (!response.ok) {
+        throw new Error("Erro ao buscar os produtos na API.")
+    }
+
+    return response.json()
+}
+
+
+// API para buscar produto por ID
+export const fetchProductId = async (id: string | string[]) => {
+    const responseId = await fetch(`${BASE_URL}/products/${id}`)
+
+    if (responseId.ok) {
+        throw new Error("Erro ao buscar um produto na API.")
+    }
+
+    return responseId.json()
+}
