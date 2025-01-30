ccfc90d0a1b980613938d69bff7e6879a336fff1
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-01-30 16:00:34 -0300

    style: criação do header, footer e alteraçao da fonte principal

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,13 +10,8 @@ const Footer = () => {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="bg-slate-100">
-            <section className="text-center">
-                <h2 className="font-semibold">Monti.</h2>
-                <p>Monti © <span>{currentYear}</span> - Todos os direitos reservados.</p>
-            </section>
-
-            <section className="flex gap-3 justify-center mt-2">
+        <footer className="border-t-2 mt-10 pt-4 px-4 rounded-t-[30px]">
+            <section className="flex justify-center gap-8 mb-7">
                 <div className="flex gap-1 transition-colors hover:text-pink-600 cursor-pointer">
                     <Instagram />
                     <p>Instagram</p>
@@ -27,45 +22,51 @@ const Footer = () => {
                 </div>
             </section>
 
-            <section>
-                <h2 className="font-semibold">Empresa</h2>
-                <Accordion type="single" collapsible className="w-full">
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger>Sobre nós</AccordionTrigger>
-                        <AccordionContent>
-                            Uma loja simples de e-commerce.
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-2">
-                        <AccordionTrigger>Vagas</AccordionTrigger>
-                        <AccordionContent>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium architecto alias deserunt beatae, enim, adipisci atque consequatur necessitatibus deleniti maiores quam nisi? Nostrum harum sunt animi eligendi porro nisi deleniti.
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-3">
-                        <AccordionTrigger>Entregas</AccordionTrigger>
-                        <AccordionContent>
-                            Entregamos para todo o Brasil.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-            </section>
+            <div className="md:flex md:gap-10 md:mb-10">
+                <section className="md:w-full">
+                    <h2 className="font-semibold text-lg">Empresa</h2>
+                    <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Sobre nós</AccordionTrigger>
+                            <AccordionContent>
+                                Uma loja simples de e-commerce.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger>Vagas</AccordionTrigger>
+                            <AccordionContent>
+                                No momento não temos vagas, mas peço que acompanhe as nossas redes sociais.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>Entregas</AccordionTrigger>
+                            <AccordionContent>
+                                Entregamos para todo o Brasil.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </section>
 
-            <section>
-                <h2 className="font-semibold">Contato</h2>
-                <div className="flex gap-2">
-                    <Phone/>
-                    <p className="text-sm">+55 01 91234-5678</p>
-                </div>
-                <div className="flex gap-2">
-                    <AtSign/>
-                    <p>contato@monti.com.br</p>
-                </div>
-                <div className="flex gap-2">
-                    <MapPin/>
-                    <p>Rua das amendoeras, 23, Centro - São Luís - MA</p>
-                </div>
-            </section>
+                <section className="mt-5 mb-6 md:w-full md:mt-0">
+                    <h2 className="font-semibold text-lg mb-3">Contato</h2>
+                    <div className="flex flex-col gap-3">
+                        <div className="flex gap-2 hover:text-slate-600">
+                            <Phone />
+                            <p className="text-sm">+55 01 91234-5678</p>
+                        </div>
+                        <div className="flex gap-2 hover:text-slate-600">
+                            <AtSign />
+                            <a href="mailto:contato@monti.com.br">contato@monti.com.br</a>
+                        </div>
+                        <div className="flex gap-2 hover:text-slate-600">
+                            <MapPin />
+                            <p>Rua das amendoeras, 23, Centro - São Luís - MA</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <p className="text-center">Monti. © <span>{currentYear}</span> - Todos os direitos reservados.</p>            
         </footer>
     )
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,7 +10,7 @@ import { ShoppingBag } from "lucide-react"
 const Header = () => {
     return (
         <header className="sticky z-10 top-3">
-            <div className="flex justify-between px-4 py-2 rounded-md mx-2 items-center drop-shadow-md bg-slate-50 mb-10">
+            <div className="flex justify-between px-4 py-2 rounded-md mx-2 items-center drop-shadow-md bg-white mb-10">
                 <h1 className="font-semibold text-xl cursor-pointer hover:text-slate-700">Monti.</h1>
                 <TooltipProvider>
                     <Tooltip>
