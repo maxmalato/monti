abe8a1c8a1d05b53abd8df7375b8114d99db39d8
maxmalato <max.malato@hotmail.com>
2025-02-19 15:14:49 -0300

    feat: categories.tsx: gerenciado de estado quando é selecionado uma categoria.

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -7,20 +7,31 @@ interface CategoriesProps {
 
 export default function Categories({ products, onSelectCategory }: CategoriesProps) {
     const [categories, setCategories] = useState<string[]>([])
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
 
     useEffect(() => {
         const uniqueCategories = Array.from(new Set(products.map(product => product.category)))
         setCategories(uniqueCategories)
     }, [products])
 
+    const handleCategoryClick = (category: string | null) => {
+        setSelectedCategory(category)
+        onSelectCategory(category)
+    }
+
     return (
-        <div className="flex justify-center space-x-2">
-            <button onClick={() => onSelectCategory(null)} className="px-3 py-1 bg-gray-300 rounded">Todos</button>
+        <div className="flex justify-center space-x-4 px-2">
+            <button
+                onClick={() => handleCategoryClick(null)}
+                className={`px-3 py-1 rounded ${selectedCategory === null ? 'bg-slate-700 text-white' : 'bg-gray-300'}`}
+            >
+                Todos
+            </button>
             {categories.map(category => (
                 <button
                     key={category}
-                    onClick={() => onSelectCategory(category)}
-                    className="px-3 py-1 bg-gray-200 rounded hover:bg-gray-400"
+                    onClick={() => handleCategoryClick(category)}
+                    className={`border-b-2 hover:border-gray-200 ${selectedCategory === category ? 'border-slate-700 text-slate-700' : 'border-gray-300'}`}
                 >
                     {category}
                 </button>
--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -25,44 +25,45 @@ export default function ProductList({ products }: ProductListProps) {
     }, [selectedCategory, products])
 
     return (
-        <main className="flex flex-col items-center gap-8 w-full">
+        <>
             <Categories products={products} onSelectCategory={setSelectedCategory} />
+            <main className="text-center">
+                {filteredProducts.length === 0 ? (
+                    <p>Nenhum produto encontrado.</p>
+                ) : (
+                    <ul className="flex flex-col gap-4 items-center my-10 px-3 md:grid md:grid-cols-2 md:gap-3 md:place-items-center lg:grid-cols-3">
+                        {filteredProducts.map((product) => {
+                            const alreadyInCart = isInCart(product.id)
 
-            {filteredProducts.length === 0 ? (
-                <p>Nenhum produto encontrado.</p>
-            ) : (
-                <ul className="flex flex-col items-center gap-4 w-full">
-                    {filteredProducts.map((product) => {
-                        const alreadyInCart = isInCart(product.id)
+                            return (
+                                <section key={product.id} className="flex flex-col items-center border p-3 gap-4 w-3/4 md:w-fit">
+                                    <h3 className="self-start p-2 bg-white rounded-md text-sm drop-shadow-md">{product.category}</h3>
+                                    <h1 className="font-semibold text-xl line-clamp-1">{product.title}</h1>
 
-                        return (
-                            <section key={product.id} className="flex flex-col items-center border p-3 w-3/4 gap-4">
-                                <h3 className="self-start p-2 bg-white rounded-md text-sm drop-shadow-md">{product.category}</h3>
-                                <h1 className="font-semibold text-xl line-clamp-1">{product.title}</h1>
-
-                                <Image src={product.thumbnail} alt={product.title} width={500} height={500} />
-                                <p className="text-center line-clamp-2">{product.description}</p>
-                                <div className="flex items-center justify-around w-full">
-                                    <p className="text-2xl font-semibold border-b-2 px-2 transition-colors hover:border-slate-900">$ {product.price}</p>
-                                    <Link href={`/products/${product.id}`}>
-                                        <Button variant="link">Mais detalhes</Button>
-                                    </Link>
-                                </div>
-                                <Button
-                                    variant="default"
-                                    className="w-full mt-4"
-                                    onClick={() => addToCart(product)}
-                                    disabled={alreadyInCart}
-                                >
-                                    <ShoppingCart /> {alreadyInCart ? "Adicionado" : "Adicionar no carrinho"}
-                                </Button>
-                            </section>
-                        )
-                    })}
-                </ul>
-            )}
+                                    <Image src={product.thumbnail} alt={product.title} width={500} height={500} />
+                                    <p className="text-center line-clamp-2">{product.description}</p>
+                                    <div className="flex items-center justify-around w-full">
+                                        <p className="text-2xl font-semibold border-b-2 px-2 transition-colors hover:border-slate-900">$ {product.price}</p>
+                                        <Link href={`/products/${product.id}`}>
+                                            <Button variant="link">Mais detalhes</Button>
+                                        </Link>
+                                    </div>
+                                    <Button
+                                        variant="default"
+                                        className="w-full mt-4"
+                                        onClick={() => addToCart(product)}
+                                        disabled={alreadyInCart}
+                                    >
+                                        <ShoppingCart /> {alreadyInCart ? "Adicionado" : "Adicionar no carrinho"}
+                                    </Button>
+                                </section>
+                            )
+                        })}
+                    </ul>
+                )}
 
 
-        </main>
+            </main>
+        </>
     )
 }
